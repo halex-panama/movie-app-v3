d974ee45009903fd34cb62af996660ad2ad80f4c
alexx155 <halex.panama@gmail.com>
2024-02-18 11:51:59 +0700

    add API and config

---
+++ src/API.js
@@ -0,0 +1,24 @@
+import { SEARCH_BASE_URL, POPULAR_BASE_URL, API_KEY, API_URL } from "./config";
+
+const apiSettings = {
+  fetchMovies: async (searchTerm, page) => {
+    const endpoint = searchTerm
+      ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}`
+      : `${POPULAR_BASE_URL}&page=${page}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetchMovie: async (movieId) => {
+    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetchCredits: async (movieId) => {
+    const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    return await (await fetch(creditsEndpoint)).json();
+  },
+  fetchVideos: async (movieId) => {
+    const videosEndpoint = `${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`;
+    return await (await fetch(videosEndpoint)).json();
+  },
+};
+
+export default apiSettings;
---
+++ src/helpers.js
@@ -0,0 +1,33 @@
+import dayjs from "dayjs";
+
+// Convert time to hours and minutes
+export const calcTime = (time) => {
+  const hours = Math.floor(time / 60);
+  const mins = time % 60;
+  return `${hours}h ${mins}m`;
+};
+// Convert a number to money formatting
+export const convertMoney = (money) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+  return formatter.format(money);
+};
+
+export const formatRating = (rating) => {
+  const fixedRating = rating?.toFixed(1);
+  return fixedRating;
+};
+
+export const formatReleaseDate = (date) => {
+  const releaseDate = dayjs(date).format("MMM D, YYYY");
+  return releaseDate;
+};
+
+//session storage
+export const isPersistedState = (stateName) => {
+  const sessionState = sessionStorage.getItem(stateName);
+  return sessionState && JSON.parse(sessionState);
+};
