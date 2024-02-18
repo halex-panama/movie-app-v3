666dc424bb92ab6ca0d35749974ec8d4e5a714f3
alexx155 <halex.panama@gmail.com>
2024-02-18 17:34:50 +0700

    add changes to api config

--- src/API.js
+++ src/API.js
@@ -1,22 +1,20 @@
-import { SEARCH_BASE_URL, POPULAR_BASE_URL, API_KEY, API_URL } from "./config";
-
 const apiSettings = {
   fetchMovies: async (searchTerm, page) => {
     const endpoint = searchTerm
-      ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}`
-      : `${POPULAR_BASE_URL}&page=${page}`;
+      ? `https://movie-app-server-eeb0.onrender.com/search?query=${searchTerm}&page=${page}`
+      : `https://movie-app-server-eeb0.onrender.com/popular?page=${page}`;
     return await (await fetch(endpoint)).json();
   },
   fetchMovie: async (movieId) => {
-    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+    const endpoint = `https://movie-app-server-eeb0.onrender.com/info/${movieId}`;
     return await (await fetch(endpoint)).json();
   },
   fetchCredits: async (movieId) => {
-    const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    const creditsEndpoint = `https://movie-app-server-eeb0.onrender.com/credits/${movieId}`;
     return await (await fetch(creditsEndpoint)).json();
   },
   fetchVideos: async (movieId) => {
-    const videosEndpoint = `${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`;
+    const videosEndpoint = `https://movie-app-server-eeb0.onrender.com/videos/${movieId}`;
     return await (await fetch(videosEndpoint)).json();
   },
 };
